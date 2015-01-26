448179465244160745f90c9561f58e2c2992468f
hbc <bcxxxxxx@gmail.com>
2015-01-26 17:07:36 +0800

    Add bus query.

--- gzbus/__init__.py
+++ gzbus/__init__.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 
 from .extract import extract_bus_routine
+from .query import query_routine, query_realtime_routine
 
-__all__ = ['extract_bus_routine']
+__all__ = ['extract_bus_routine', 'query_routine', 'query_realtime_routine']
---
+++ gzbus/query.py
@@ -0,0 +1,58 @@
+# coding: utf-8
+
+'''
+    gzbus.query
+    ~~~~~~~~~~~
+
+    Query bus routines.
+'''
+
+import requests
+from pyquery import PyQuery as pq
+
+from .data.gzyyjt import buses
+from .extract import extract_bus_routine
+
+
+REALTIME_ROUTINE_QUERY = 'http://www.gzyyjt.com/Bus_station.aspx'
+
+
+def _get_realtime_page(bus_name, bid, cur_station):
+    query = {
+        'bid': bid,
+        'sn': cur_station,
+        'bn': bus_name
+    }
+    resp = requests.get(REALTIME_ROUTINE_QUERY, params=query)
+
+    if not resp.ok:
+        return
+
+    return pq(resp.content)
+
+
+def query_routines(bus_name):
+    '''Get bus routines from name.
+
+    TODO support fuzzy matching.
+    '''
+    return buses.get(bus_name)
+
+
+def query_realtime_routine(bus_name, cur_station=None):
+    '''Get real time routine.
+
+    TODO support fuzzy matching.
+    '''
+    routines = query_routines(bus_name)
+    if not routines:
+        return
+
+    rv = []
+    for routine in routines:
+        bid = routine['bid']
+        _cur_station = cur_station or routine['starting_station']
+        page = _get_realtime_page(bus_name, bid, _cur_station)
+        rv.append(extract_bus_routine(page))
+
+    return rv
